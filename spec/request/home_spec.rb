fd5f9c352e1a10aefe6d0511a2282bc6bdb81da0
J. William Ashton <William@AshtonFam.org>
2014-02-12 08:54:36 -0500

    Test root render status code

---
+++ spec/request/home_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "Home Page" do
+  describe "Get /" do
+    
+    context "empty database" do
+      
+      before (:each) do
+        visit '/'
+      end
+      
+      it "render success" do
+        expect(page.status_code).to be 200
+      end
+      
+    end
+    
+  end
+end
