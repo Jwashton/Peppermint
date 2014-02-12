2661a8aa36fb901e3f60a6441d354e14c4887f6c
J. William Ashton <William@AshtonFam.org>
2014-02-12 11:55:19 -0500

    Add spec for the User has many Players association

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,8 @@ describe User do
     it { expect(user).to validate_presence_of   :username }
     it { expect(user).to validate_uniqueness_of :username }
     
+    it { expect(user).to have_many :players }
+    
     it "saves attributes" do
       user.save!
       expect(user).to be_valid
