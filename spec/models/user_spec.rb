f570029fb0f55e996d3c542010b10920843ea61e
J. William Ashton <William@AshtonFam.org>
2014-02-12 10:21:06 -0500

    Add spec to validate the presence of the username

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,16 @@ require 'spec_helper'
 describe User do
   
   let(:user) { user = User.new(username: "Fred") }
-  it "saves attributes" do
-    user.save!
-    expect(user).to be_valid
+  
+  describe "attributes" do
+    
+    it { expect(user).to validate_presence_of :username }
+    
+    it "saves attributes" do
+      user.save!
+      expect(user).to be_valid
+    end
+    
   end
   
 end
