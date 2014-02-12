91aa2ef5637d474566cfe62415eb3f101faa74a2
J. William Ashton <William@AshtonFam.org>
2014-02-12 10:10:56 -0500

    Refactor user test to assist the addition of future tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe User do
   
+  let(:user) { user = User.new(username: "Fred") }
   it "saves attributes" do
-    user = User.new(username: "Fred")
     user.save!
     expect(user).to be_valid
   end
